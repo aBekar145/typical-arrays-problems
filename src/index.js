941d61177a03af0b8b21aebe764d0d0a05dda9f6
Александр <aBekar145@gmail.com>
2021-03-13 23:49:57 +0300

    feat: test

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 
 exports.min = function min (array) {
-  return 0;
+  let result = 0;
+  if  (!array || array.length === 0) {
+    result = 0;
+  } else {
+    result = Math.min(...array);
+  }
+  return result;
 }
 
 exports.max = function max (array) {
-  return 0;
+  let result = 0;
+  if (!array || array.length === 0) {
+    result = 0;
+  } else {
+    result = Math.max(...array);
+  }
+  return result;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let result = 0;
+  if (!array || !Array.isArray(array) || array.length === 0) {
+    result = 0;
+    return result;
+  } else {
+    result = array.reduce((accum, item) => {
+      if (item > 0 || item < 0) {
+        accum += item;
+      }
+      return accum;
+    });
+    return result / array.length;
+  }
 }
